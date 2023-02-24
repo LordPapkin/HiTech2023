9948b392789cc4b27c8f008c10811363513a8d0a
Tofikson <filipswistak@gmail.com>
2023-02-24 23:56:39 +0100

    Main Menu adjustments, Play button not ready

--- Assets/Button Scripts/PlayButton.cs
+++ Assets/Button Scripts/PlayButton.cs
@@ -7,14 +7,25 @@ public class PlayButton : MonoBehaviour
 {
     public Button yourButton;
 
+    public GameObject menuNothing = GameObject.Find("MenuNoButtonsClicked");
+    public GameObject menuPlay = GameObject.Find("MenuPlayClicked").GetComponent<GameObject>();
+
+
     void Start()
     {
+        menuNothing.SetActive(true);
+        menuPlay.SetActive(false);
         Button btn = yourButton.GetComponent<Button>();
         btn.onClick.AddListener(TaskOnClick);
     }
 
-    void TaskOnClick()
+    public void TaskOnClick()
     {
         Debug.Log("You have clicked the button!");
     }
+    public void OnClick()
+    {
+        
+    }
+
 }
