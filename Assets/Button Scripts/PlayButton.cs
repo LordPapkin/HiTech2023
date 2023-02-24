87cab7148646d3aecbaf9b666ae3520642f65782
Tofikson <filipswistak@gmail.com>
2023-02-25 00:40:13 +0100

    Addes Options Menu, adjusted Main Menu functions

--- Assets/Button Scripts/PlayButton.cs
+++ Assets/Button Scripts/PlayButton.cs
@@ -7,14 +7,17 @@ public class PlayButton : MonoBehaviour
 {
     public Button yourButton;
 
-    public GameObject menuNothing = GameObject.Find("MenuNoButtonsClicked");
-    public GameObject menuPlay = GameObject.Find("MenuPlayClicked").GetComponent<GameObject>();
+    public GameObject menuNothing;
+    public GameObject menuPlay;
+    public GameObject menuOptions;
 
 
     void Start()
     {
         menuNothing.SetActive(true);
         menuPlay.SetActive(false);
+        menuOptions.SetActive(false);
+
         Button btn = yourButton.GetComponent<Button>();
         btn.onClick.AddListener(TaskOnClick);
     }
@@ -22,10 +25,15 @@ public class PlayButton : MonoBehaviour
     public void TaskOnClick()
     {
         Debug.Log("You have clicked the button!");
+        menuNothing.SetActive(false);
+        menuPlay.SetActive(true);
+        menuOptions.SetActive(false);
     }
-    public void OnClick()
-    {
-        
-    }
+    //public void OnClick()
+    //{
+    //    menuNothing.SetActive(false);
+    //    menuPlay.SetActive(true);
+    //    menuOptions.SetActive(false);
+    //}
 
 }
