8fcb89bea225041153d27255bcbd3b023fea7627
LordPapkin <papkin2013@gmail.com>
2023-02-26 13:30:31 +0100

    turret manager

---
+++ Assets/Scripts/TurretManager.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class TurretManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] turretsPrefabs;
+    [SerializeField] private GameObject[] turretPads;
+
+    [SerializeField] private float minTimeToSpawnNextTurret;
+    [SerializeField] private float maxTimeToSpawnNextTurret;
+
+    private void Start()
+    {
+        StartCoroutine(SpawningTurretsCor());
+    }
+
+    private IEnumerator SpawningTurretsCor()
+    {
+        while (true)
+        {
+            SpawnRandomTurret();
+            yield return new WaitForSeconds(Random.Range(minTimeToSpawnNextTurret, maxTimeToSpawnNextTurret));
+        }
+    }
+
+    private void SpawnRandomTurret()
+    {
+        var randomPad = turretPads[Random.Range(0, turretPads.Length)];
+        Instantiate(turretsPrefabs[Random.Range(0, turretsPrefabs.Length)], randomPad.transform.position,
+            Quaternion.identity, transform);
+    }
+}
