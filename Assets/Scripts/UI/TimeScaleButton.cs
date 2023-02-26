0a1703ac1b23a48400dcd6e0f19053edbc4e5434
LordPapkin <papkin2013@gmail.com>
2023-02-26 09:32:59 +0100

    fixed timescale buttons

--- Assets/Scripts/UI/TimeScaleButton.cs
+++ Assets/Scripts/UI/TimeScaleButton.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,52 @@ using UnityEngine;
 
 public class TimeScaleButton : MonoBehaviour
 {
-    
+    private enum GameSpeed
+    {
+        Normal,
+        Fast,
+        Paused
+    }
+
+    private static bool isFast;
+
+    public void SetPause()
+    {
+        ChangeSpeed(GameSpeed.Paused);
+    }
+
+    public void SetFast()
+    {
+        ChangeSpeed(GameSpeed.Fast);
+    }
+
+    public void SetNormal()
+    {
+        ChangeSpeed(GameSpeed.Normal);
+    }
+
+    public void ResumeGame()
+    {
+        ChangeSpeed(isFast ? GameSpeed.Fast : GameSpeed.Normal);
+    }
+
+    private void ChangeSpeed(GameSpeed gameSpeed)
+    {
+        switch (gameSpeed)
+        {
+            case (GameSpeed.Normal):
+                Time.timeScale = 1f;
+                isFast = false;
+                break;
+            case (GameSpeed.Fast):
+                Time.timeScale = 1.7f;
+                isFast = true;
+                break;
+            case (GameSpeed.Paused):
+                Time.timeScale = 0f;
+                break;
+                
+        }
+    }
+
 }
