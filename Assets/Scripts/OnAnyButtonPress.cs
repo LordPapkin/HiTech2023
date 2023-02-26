877a52cbc95459317156fd868855a74a417f6290
PPGKnight <kuba.hankus@gmail.com>
2023-02-26 14:39:11 +0100

    Working UI

--- Assets/Scripts/OnAnyButtonPress.cs
+++ Assets/Scripts/OnAnyButtonPress.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OnAnyButtonPress : MonoBehaviour
 {
+    public void MoveToScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
     void Update()
     {
         if (Input.anyKey)
         {
-            Debug.Log("Key has been pressed");
-            return;
+            MoveToScene(0);
         }
     }
 }
